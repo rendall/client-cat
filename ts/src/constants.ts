898b519b5b0045d052993f22fd5faaae3052a367
Rendall <rendall@gmail.com>
2020-08-10 14:28:02 +0300

    Change the API domain from .com to .app

--- ts/src/constants.ts
+++ ts/src/constants.ts
@@ -1,2 +1,2 @@
 export const CAT_IMG = "/img/cat-300px.png"
-export const BREEDS_API = 'https://focused-elion-17290b.netlify.com/.netlify/functions/breed'
+export const BREEDS_API = 'https://focused-elion-17290b.netlify.app/.netlify/functions/breed'
